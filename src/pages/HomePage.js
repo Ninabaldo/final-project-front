c090c307dcc9c8030b17b1e37aca778e12938e3e
Ninabaldo <nbaldorousseau@gmail.com>
2021-10-04 11:39:04 +0200

    home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,26 +1,17 @@
 import "../components/scss/Home.scss"
+import homeImage from "../img/home.jpeg"
 
 
 function HomePage() {
   return (
     <>
-    <h2>Put somme colors<br></br> in your<br></br>
+    <h2>Put somme colors<br></br>
+     in your<br></br>
      digital world</h2>
-
-    <div class="content">
-  <div class="content__container">
-    <p class="content__container__text">
-      Color
-    </p>
+     <img src={homeImage} className="styleimage"/>
+          
     
-    <ul class="content__container__list">
-      <li class="content__container__list__item">your world!</li>
-      <li class="content__container__list__item">your mind!</li>
-      <li class="content__container__list__item">your project!</li>
-      <li class="content__container__list__item">ID :)!</li>
-    </ul>
-  </div>
-</div>
+ 
 
 
 
