5f3b946efd2f6e7729cbf4cbb59876b9a104ebb9
Ninabaldo <nbaldorousseau@gmail.com>
2021-10-06 16:51:33 +0200

    last

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,4 @@
-import login from "../pages/LoginPage"
-import signup from "../pages/SignupPage"
-import ranking from "../pages/Ranking"
-import paleteColors from "../pages/SimpleColor"
-import paleteCombi from "../pages/PaleteColors"
+
 import "./scss/NavBar.scss";
 import { AuthContext } from "./../context/auth.context";
 import { useContext } from "react";
