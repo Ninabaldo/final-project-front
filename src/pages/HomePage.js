317aaf0573ee490ada355571b73d582eb8755536
Ninabaldo <nbaldorousseau@gmail.com>
2021-10-07 17:57:48 +0200

    log in log out

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,27 +2,32 @@ import "../components/scss/Home.scss"
 import homeImage from "../img/home.jpeg"
 import Footer from "../components/Footer";
 import { useHistory } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+import { useEffect, useState, useContext } from "react";
 
 
 function HomePage() {
   const history = useHistory()
-
+  const { user, isLoggedIn } = useContext(AuthContext);
   return (
 
     <>
 
+
       <h3 className="home">Put somme colors<br></br>
         in your<br></br>
         digital world </h3>
+      {!isLoggedIn ?
+        <button className="homepage" onClick={() => { history.push("/login") }}>
+          Login
 
-      <button className="homepage" onClick={() => { history.push("/login") }}>
-        Login
-
-      </button>
-      <button className="homepage1" onClick={() => { history.push("/signup") }}>
-        Signup
-      </button>
-
+        </button>
+        : null}
+      {!isLoggedIn ?
+        <button className="homepage1" onClick={() => { history.push("/signup") }}>
+          Signup
+        </button>
+        : null}
       <img src={homeImage} className="styleimage" />
 
       <Footer></Footer>
